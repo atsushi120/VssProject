a40c24bf54f5fdb8f6b6ea190d38fba1ff16a09a
Atsushi Osaki <atsushi_osaki@hotmail.com>
2015-06-29 20:30:58 -0700

    Initial commit of the VSS project

---
+++ GitProject.cpp
@@ -0,0 +1,14 @@
+// GitProject.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace::std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
