f5e492c56f0e99d34179229a82e2bb9857533b82
Atsushi Osaki <atsushi_osaki@hotmail.com>
2015-06-30 20:37:55 -0700

    Added find routine using iterator

--- GitProject.cpp
+++ GitProject.cpp
@@ -3,6 +3,8 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <list>
+#include <algorithm>
 
 using namespace::std;
 
@@ -16,6 +18,16 @@ public:
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "Hello World!" << endl;
+
+	list<int> mylist;
+	mylist.push_back(101);
+	mylist.push_back(203);
+	mylist.push_back(304);
+	mylist.push_back(423);
+	
+	list<int>::iterator itFind = find(mylist.begin(), mylist.end(), 304);
+	cout << "Found : " << *(itFind) << endl;
+
 	return 0;
 }
 
